f68e012288d07f4cb1cdd98c66e6477f7ab752da
Sagnik Basu <basusagnik1995@gmail.com>
2019-09-28 19:46:12 +0530

    Logging infra

---
+++ core/logging.rs
@@ -0,0 +1,24 @@
+use std::format;
+
+struct Log
+{
+
+}
+
+impl Log {
+
+    fn D /* Debug Logs */ 
+    {
+
+    }
+
+    fn I /* Info Logs */
+    {
+
+    }
+
+    fn E /*Error Logs */
+    {
+
+    }
+}
