422f9be99568cb59263233bd45ef982cdfa19db4
sagniknitr <basusagnik1995@gmail.com>
2019-10-29 14:51:16 +0530

    color codes

---
+++ core/color.rs
@@ -0,0 +1,45 @@
+enum RGB
+{
+    RGB = 0,
+    RGB888 = 3,
+    RGB8888 = 4
+}
+
+enum LUV
+{
+    RGB = 0,
+    RGB888 = 3,
+    RGB8888 = 4
+
+}
+
+enum Y
+{
+    NV12,
+    NV21,
+    YUV420,
+    YUV422,
+    YUV440,
+    YCbCr
+}
+
+
+enum B8
+{
+    U8,
+    S8,
+    QU_8, /* Q format Q.8 */
+    QS_7  /* Q format Q.7 */
+    
+}
+
+enum B16
+{
+    U16,
+    S16,
+    QU8_8, /* Q format Q8.8 */
+    QS7_8, /* Q format Q7.8 */
+    QU_16, /* Q format Q.16 */
+    QS_15  /* Q format Q.15 */
+}
+
